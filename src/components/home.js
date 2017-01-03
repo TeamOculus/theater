4404f624052055d8c3561c202d45ae737d11a3bc
Henry Qi <henryqi222@gmail.com>
2017-01-03 11:48:27 -0700

    working routing

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.goToTheater = this.goToTheater.bind(this);
+  }
+
   render() {
     return (
-      <div>
-        this is from home
+      <div className="show-vr-div">
+        <a-scene>
+          <a-assets>
+            <img id="theaterlink" src="http://localhost:8080/src/assets/models/theater/theaterlink.jpg" />
+          </a-assets>
 
-        <Link to="/theater"><button type="button">Go to Theater</button></Link>
+          <a-plane id="theaterlink2" src="#theaterlink" height="10" width="10" position="0 0 -10"></a-plane>
+
+          <a-entity position="0 1.8 4">
+            <a-entity camera="" look-controls="" mouse-cursor="">
+              <a-entity id="cursor" position="0 0 -1" geometry="primitive: ring; radiusOuter: 0.016; radiusInner: 0.01" material="color: #FFF;" cursor="fuse: true; maxDistance: 8">
+                <a-animation begin="fusing" easing="ease" attribute="scale" fill="none" from="1 1 1" to="0 0 0" dur="1500"></a-animation>
+              </a-entity>
+            </a-entity>
+          </a-entity>
+
+
+        </a-scene>
       </div>
     )
+  }
+
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  componentDidMount() {
+    let goToTheaterPointer = this.goToTheater;
+    document.querySelector('#theaterlink2').addEventListener('click', function () {
+      console.log('I was clicked!');
+      goToTheaterPointer();
+    });
+  }
+
+  goToTheater() {
+    this.context.router.push("theater")
   }
 }
 
--- src/components/theater.js
+++ src/components/theater.js
@@ -8,14 +8,14 @@ class Theater extends React.Component {
 {/*
         <a-scene>
           <a-assets>
-            <video id="nowplaying" autoplay loop="true" src="http://localhost:8080/src/videos/beebz.mp4" />
+            <video id="nowplaying" autoplay loop="true" src="http://localhost:8080/src/assets/videos/beebz.mp4" />
           </a-assets>     
           <a-video src="#nowplaying" width="16" height="9" position="0 0 -20"></a-video>
         </a-scene>
 */}  
 
 {/*
-        <audio id="audioElement" src="http://localhost:8080/src/videos/morebeebz.mp3"></audio>
+        <audio id="audioElement" src="http://localhost:8080/src/assets/videos/morebeebz.mp3"></audio>
         <button onClick={this.play}>Play the Audio</button>
         <button onClick={this.pause}>Pause the Audio</button>
         <button onClick={this.increaseVol}>Increase Volume</button>
@@ -24,16 +24,16 @@ class Theater extends React.Component {
 
       <a-scene>
         <a-assets>
-          <a-asset-item id="theater-model" src="http://localhost:8080/src/models/model4.dae"></a-asset-item>
-          <a-asset-item id="ferrari" src="http://localhost:8080/src/models/ferrarif12.dae"></a-asset-item>
-          <a-asset-item id="helicopter" src="http://localhost:8080/src/models/helicopter.dae"></a-asset-item>
+          <a-asset-item id="theater-model" src="http://localhost:8080/src/assets/models/theater/model4.dae"></a-asset-item>
+          <a-asset-item id="ferrari" src="http://localhost:8080/src/assets/models/carshow/ferrarif12.dae"></a-asset-item>
+          <a-asset-item id="helicopter" src="http://localhost:8080/src/assets/models/carshow/helicopter.dae"></a-asset-item>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
         </a-assets>
 
         <a-sky src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg"></a-sky>
         <a-plane src="#groundTexture" rotation="-90 0 0" width="30" height="30" repeat="35 35"></a-plane>
 
-        <a-entity collada-model="#theater-model" position="0 1 0"></a-entity>
+        <a-entity collada-model="#theater-model" position="0 0 0"></a-entity>
         <a-entity collada-model="#ferrari" position="0 0 -2" rotation="0 90 0"></a-entity>
         <a-entity collada-model="#helicopter" position="3 8 -10"></a-entity>
 
