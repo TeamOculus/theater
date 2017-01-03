7aa1aebbc2346518360961fabc52b66f7f6b9d25
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-01-03 10:20:35 -0700

    Doing some testing with changes

--- src/components/theater.js
+++ src/components/theater.js
@@ -24,9 +24,18 @@ class Theater extends React.Component {
 
       <a-scene>
         <a-assets>
-          <a-asset-item id="theater-model" src="http://localhost:8080/src/models/model.dae"></a-asset-item>
+          <a-asset-item id="theater-model" src="http://localhost:8080/src/models/model4.dae"></a-asset-item>
+          <a-asset-item id="ferrari" src="http://localhost:8080/src/models/ferrarif12.dae"></a-asset-item>
+          <a-asset-item id="helicopter" src="http://localhost:8080/src/models/helicopter.dae"></a-asset-item>
+          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
         </a-assets>
-        <a-entity collada-model="#theater-model"></a-entity>
+
+        <a-sky src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg"></a-sky>
+        <a-plane src="#groundTexture" rotation="-90 0 0" width="30" height="30" repeat="35 35"></a-plane>
+
+        <a-entity collada-model="#theater-model" position="0 1 0"></a-entity>
+        <a-entity collada-model="#ferrari" position="0 0 -2" rotation="0 90 0"></a-entity>
+        <a-entity collada-model="#helicopter" position="3 8 -10"></a-entity>
 
         <a-entity position="10 0 -10">
           <a-camera></a-camera>
