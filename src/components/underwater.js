b0c069dea46f65ba546f7dd2414d465ef29cdb74
yangho1231 <yangho1231@gmail.com>
2017-01-04 10:47:28 -0700

    merge conflict

--- src/components/underwater.js
+++ src/components/underwater.js
@@ -6,7 +6,15 @@ class Underwater extends React.Component {
       <div className="show-vr-div">
 
         <a-scene>
-            
+            <a-assets>
+              <a-asset-item id='background' src='http://localhost:8080/src/assets/models/underwater/model1.dae'></a-asset-item>
+            </a-assets>
+
+            <a-entity collada-model="#background" position="0 0 0"></a-entity>
+
+            <a-entity position="0 0 0">
+              <a-camera></a-camera>
+            </a-entity>
         </a-scene>
 
       </div>
