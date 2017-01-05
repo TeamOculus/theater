b2295a4048a3b51c5fc4507b9b465fa89e549d00
yangho1231 <yangho1231@gmail.com>
2017-01-05 12:19:42 -0700

    Fixed the bubble flow

--- src/components/underwater.js
+++ src/components/underwater.js
@@ -1,32 +1,40 @@
 import React from 'react';
 
 class Underwater extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bubble: []
+    }
+  }
+  componentWillMount() {
+     var i = 0;
+     while(i < 500) {
+      let x = Math.random() * 100 - 50;
+      let y = Math.random() * 10;
+      let z = Math.random() * 100 - 50;
+      let bubble = this.state.bubble;
+      bubble.push(<a-entity key={i} collada-model="#bubble" position={`${x} ${y} ${z}`} scale = "0.01 0.01 0.01"><a-animation attribute="position" to={`${x + Math.random() * 2 - 1} ${y + 5} ${z}`} dur={`${Math.random() * 12000}`} begin="4000" repeat="indefinite"></a-animation>
+          </a-entity>);
+       i++;
+     }
+
+
+  }
   render(){
-      var i = 0;
       var j = 0
-      var bubble = [];
       var seaweed = [];
+
       let x, y, z;
-      while(j < 1) {
-        x = Math.random() * 120 -60;
+      while(j < 200) {
+        x = Math.random() * 100 -50;
         y = 0;
-        z = Math.random() * 120 -60;
+        z = Math.random() * 100 -50;
         seaweed.push(<a-entity key={j} collada-model="#seaweed" position={`${x} ${y} ${z}`}></a-entity> );
         j++;
       }
-      
-      while(i < 1) {
-        x = Math.random() * 12 - 6;
-        y = Math.random() * 10;
-        z = Math.random() * 12 - 6;
-        bubble.push(<a-entity key={i} collada-model="#bubble" position={`${x} ${y} ${z}`} scale = "0.01 0.01 0.01"><a-animation attribute="position" to={`${x + Math.random() * 2 - 1} ${y + 5} ${z}`} dur="5000" begin="1" repeat="indefinite"></a-animation>
-            </a-entity>);
-        
-        i++;
-      }
-      setTimeout(function() {
+    
 
-      }, 1000)
     return (
       <div className="show-vr-div">
         <a-scene>
@@ -43,17 +51,24 @@ class Underwater extends React.Component {
             </a-assets>
 
             <a-entity collada-model="#background" position="0 0 0"></a-entity>
-             <a-entity collada-model='#whale' scale="1 1 1"position="30 3 -50"></a-entity>
-            <a-entity collada-model='#shark' scale="1 1 1"position="50 2 15"></a-entity>
-            <a-entity collada-model='#orca' scale="10 10 10"position="50 2 15"></a-entity>
+            <a-entity collada-model='#whale' scale="1 1 1"position="30 3 -50">
+              <a-animation attribute="position" from="10 5 -50" to="150 5 -50" repeat="indefinite" direction="alternate-reverse" dur="30000"></a-animation> 
+            </a-entity>
+            <a-entity collada-model='#shark' scale="1 1 1"position="50 2 15">
+              <a-animation attribute="position" from="70 2 15" to="10 2 15" repeat="indefinite" direction="alternate-reverse" dur="30000"></a-animation>
+              
+            </a-entity>
+           
             <a-entity collada-model='#plant1' scale="1 1 1"position="10 0 10"></a-entity>
             <a-entity collada-model='#wartortle' scale="7 7 7"position="-20 0 20" rotation="90 180 0"></a-entity>
            
          
-
             {seaweed}
-            {bubble}
-          
+            {this.state.bubble}
+            <a-entity>
+              <a-camera wasd-controls="fly: true; acceleration: 150"></a-camera>
+            </a-entity>
+           
 
 
             
