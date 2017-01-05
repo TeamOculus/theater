33d1e98bbf684f20f77cbb89ad3daeacd1c02ce8
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-01-05 12:37:37 -0700

    Added teleport back to main command center

--- src/components/space.js
+++ src/components/space.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
 class Space extends React.Component {
   constructor() {
@@ -7,12 +8,25 @@ class Space extends React.Component {
       xwingdir: true,
       directionflag: 0
     };
+    this.goHome = this.goHome.bind(this)
     this.reversedir1 = this.reversedir1.bind(this)
     this.reversedir2 = this.reversedir2.bind(this)
   }
 
+  static contextTypes = {
+    router: PropTypes.object
+  }
   componentDidMount(){
     this.reversedir1();
+    let goHomePointer = this.goHome;
+    document.querySelector('#exit').addEventListener('click', function () {
+      console.log('I was clicked!');
+      goHomePointer();
+    });
+  }
+
+  goHome() {
+    this.context.router.push("/")
   }
 
   reversedir1() {
@@ -58,28 +72,16 @@ class Space extends React.Component {
             <a-asset-item id="milleniumfalcon" src="http://localhost:8080/src/assets/models/space/milleniumfalcon/model.dae"></a-asset-item>
 
 
-            {/*<a-asset-item id="milkyway" src="http://localhost:8080/src/assets/models/space/milkyway/model.dae"></a-asset-item>
-            <a-asset-item id="earth" src="http://localhost:8080/src/assets/models/space/earth/earth.dae"></a-asset-item>
-            <a-asset-item id="saturn" src="http://localhost:8080/src/assets/models/space/saturn/model.dae"></a-asset-item>
-            <a-asset-item id="uranus" src="http://localhost:8080/src/assets/models/space/uranus/model.dae"></a-asset-item>
-            <a-asset-item id="sun" src="http://localhost:8080/src/assets/models/space/sun/model.dae"></a-asset-item>
-
+            {/*
             <a-asset-item id="enterprise1" src="http://localhost:8080/src/assets/models/space/enterprise1/model.dae"></a-asset-item>
             */}
           </a-assets>
 
           <a-sky src="#spaceTexture"></a-sky>
-          {/*<a-entity collada-model="#sun" position="5 0 0" scale="1 1 1">
-             
-          </a-entity>
-          <a-entity collada-model="#earth" position="15 0 0" sound="src: #sound; autoplay: false" look-at="#camera">
-             <a-animation attribute="rotation" dur="20000" fill="forwards" from="0 0 0" to="0 360 0" repeat="indefinite"></a-animation>
-          </a-entity>*/}
+         
           <a-sphere src="#zeuseus" radius="1" translate="0 1 0" position="-30 1 0">
             <a-animation attribute="rotation" dur="20000" direction="reverse" fill="backwards" from="0 0 0" to="0 360 0" repeat="indefinite"></a-animation>
           </a-sphere>
-          
-          {/*<a-image src="#arrow" position="0 20 0"></a-image>*/}
           
           <a-sphere src="#sun" radius="14" translate="0 1 0">
             <a-animation attribute="rotation" dur="20000" fill="forwards" from="0 0 0" to="0 360 0" repeat="indefinite"></a-animation>
@@ -134,12 +136,13 @@ class Space extends React.Component {
             <a-animation attribute="rotation" dur="20000" fill="forwards" from="0 0 0" to="0 360 0" repeat="indefinite"></a-animation>
           </a-sphere>
           
-          <a-entity collada-model="#stargate" position="20 20 0"></a-entity>
+          <a-entity id="exit" collada-model="#stargate" position="20 20 0"></a-entity>
           
           <a-entity id="xwing-anim" collada-model="#xwing" position="0 -70 20" rotation="0 270 0" sound="src: #sound; autoplay: false">
             <a-animation attribute="position" dur="20000" from="-150 -70 20" to="200 -70 20" direction="alternate" repeat="indefinite">
             </a-animation>
           </a-entity>
+
           {/*
           <a-entity collada-model="#milleniumfalcon" position="0 20 20" rotation="0 270 0">
             <a-animation attribute="position" dur="20000" from="200 20 -20" to="-150 -20 -20" direction="alternate" repeat="indefinite">
@@ -148,24 +151,15 @@ class Space extends React.Component {
           */}
           
           <a-entity position="30 -1.5 40">
-            <a-camera id="camera" look-controls wasd-controls="acceleration: 300; fly: true"></a-camera>
+            <a-camera id="camera" look-controls wasd-controls="acceleration: 300; fly: true">
+              <a-cursor color="yellow" fuse="true" fuse-timeout="3000">
+                <a-animation begin="fusing" easing="ease" attribute="scale" fill="none" from="1 1 1" to="0 0 0" dur="3000"></a-animation>
+              </a-cursor>
+            </a-camera>
           </a-entity>
         </a-scene>
         
-          {/*<a-entity collada-model="#saturn" position="90 0 -50" rotation="0 0 5" scale="1 1 0.7">
-            <a-animation attribute="rotation"
-               dur="50000"
-               fill="forwards"
-               to="0 360 0"
-               repeat="indefinite"></a-animation>
-          </a-entity>
-          <a-entity collada-model="#uranus" position="-240 10 -375" scale="15 15 15">
-            <a-animation attribute="rotation"
-               dur="50000"
-               fill="forwards"
-               to="0 360 0"
-               repeat="indefinite"></a-animation>
-          </a-entity>*/}
+        
 
 
           {/*<a-entity collada-model="#enterprise1" position="-10 -90 -100" rotation="0 180 0" scale="1 1 0.8"></a-entity>*/}
@@ -173,7 +167,6 @@ class Space extends React.Component {
       </div>
     )
   }
-
   
 }
 
