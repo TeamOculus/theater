0a8cae6ccf1de43e26c3b5a43b21d5704ffdb67b
Cameron Skidmore <cskidz99@gmail.com>
2017-01-05 11:36:27 -0700

    Flying ability added. Might disable later in favor of keeping user on the ground for the purposes of the Waldo search game.

--- src/components/city.js
+++ src/components/city.js
@@ -41,11 +41,11 @@ class City extends React.Component {
 
           <a-entity collada-model="#park2" position="29.15 0 26.77"></a-entity>
 
-          {/*<a-camera position="0 0 0">
-            <a-cursor color="yellow" fuse="true" fuse-timeout="3000">
+          <a-camera position="0 0 0" wasd-controls="fly: true">
+            {/*<a-cursor color="yellow" fuse="true" fuse-timeout="3000">
               <a-animation begin="fusing" easing="ease" attribute="scale" fill="none" from="1 1 1" to="0 0 0" dur="3000"></a-animation>
-            </a-cursor>
-          </a-camera>*/}
+            </a-cursor>*/}
+          </a-camera>
         </a-scene>
 
         
