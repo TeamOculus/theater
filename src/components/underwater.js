e81bf70455ab76665be29c6b339d6f322a2d14c2
yangho1231 <yangho1231@gmail.com>
2017-01-05 17:35:21 -0700

    Added sound and exit sign

--- src/components/underwater.js
+++ src/components/underwater.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
 class Underwater extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bubble: []
+      bubble: [],
+      directiongflag: 0
+
     }
+    this.goHome = this.goHome.bind(this);
+    this.reversedir1 = this.reversedir1.bind(this)
+    this.reversedir2 = this.reversedir2.bind(this)
   }
   componentWillMount() {
      var i = 0;
-     while(i < 500) {
-      let x = Math.random() * 100 - 50;
+     while(i < 300) {
+      let x = Math.random() * 80 - 40;
       let y = Math.random() * 10;
-      let z = Math.random() * 100 - 50;
+      let z = Math.random() * 80 - 40;
       let bubble = this.state.bubble;
-      bubble.push(<a-entity key={i} collada-model="#bubble" position={`${x} ${y} ${z}`} scale = "0.01 0.01 0.01"><a-animation attribute="position" to={`${x + Math.random() * 2 - 1} ${y + 5} ${z}`} dur={`${Math.random() * 12000}`} begin="4000" repeat="indefinite"></a-animation>
+      bubble.push(<a-entity key={i} collada-model="#bubble" position={`${x} ${y} ${z}`} scale = "0.01 0.01 0.01"><a-animation attribute="position" to={`${x + Math.random() * 2 - 1} ${y + 5} ${z}`} dur={`${Math.random() * 12000}`} begin="3000" repeat="indefinite"></a-animation>
           </a-entity>);
        i++;
      }
@@ -26,10 +32,10 @@ class Underwater extends React.Component {
       var seaweed = [];
 
       let x, y, z;
-      while(j < 200) {
-        x = Math.random() * 100 -50;
+      while(j < 100) {
+        x = Math.random() * 60 -30;
         y = 0;
-        z = Math.random() * 100 -50;
+        z = Math.random() * 60 -30;
         seaweed.push(<a-entity key={j} collada-model="#seaweed" position={`${x} ${y} ${z}`}></a-entity> );
         j++;
       }
@@ -40,33 +46,45 @@ class Underwater extends React.Component {
         <a-scene>
         
             <a-assets>
+              <audio id = "jaws" src = "http://localhost:8080/src/assets/sounds/jaws.mp3"></audio>
+              <img id="exit-blue" src="http://localhost:8080/src/assets/imgs/theaterexit.png" />
               <a-asset-item id='background' src='http://localhost:8080/src/assets/models/underwater/model1.dae'></a-asset-item>
               <a-asset-item id='bubble' src='http://localhost:8080/src/assets/models/underwater/model5.dae'></a-asset-item>
               <a-asset-item id='shark' src='http://localhost:8080/src/assets/models/underwater/shark1.dae'></a-asset-item>
               <a-asset-item id='whale' src='http://localhost:8080/src/assets/models/underwater/whale.dae'></a-asset-item>
-     
-              <a-asset-item id='plant1' src='http://localhost:8080/src/assets/models/underwater/plant1.dae'></a-asset-item>
+              <a-asset-item id='whale2' src='http://localhost:8080/src/assets/models/underwater/whale2.dae'></a-asset-item>
               <a-asset-item id='seaweed' src='http://localhost:8080/src/assets/models/underwater/seaweed.dae'></a-asset-item>
               <a-asset-item id='wartortle' src='http://localhost:8080/src/assets/models/underwater/wartortle.dae'></a-asset-item>
             </a-assets>
 
             <a-entity collada-model="#background" position="0 0 0"></a-entity>
-            <a-entity collada-model='#whale' scale="1 1 1"position="30 3 -50">
-              <a-animation attribute="position" from="10 5 -50" to="150 5 -50" repeat="indefinite" direction="alternate-reverse" dur="30000"></a-animation> 
+
+            <a-entity id="whale-anim" collada-model='#whale2' scale="20 20 20" position="70 5 -50" rotation="0 0 -15">
+              <a-animation attribute="position" from="70 5 -50" to="-5 5 -50" repeat="indefinite" direction="alternate" dur="20000"></a-animation>
             </a-entity>
-            <a-entity collada-model='#shark' scale="1 1 1"position="50 2 15">
-              <a-animation attribute="position" from="70 2 15" to="10 2 15" repeat="indefinite" direction="alternate-reverse" dur="30000"></a-animation>
+
+
+
+            <a-entity id="shark-anim"collada-model='#shark' scale="1 1 1"position="10 2 15" sound="src: #jaws; autoplay: false; on: click">
+              <a-animation attribute="position" from="10 2 15" to="50 2 15"repeat="indefinite" direction="alternate" dur="20000"></a-animation>
+              <a-sound></a-sound>
               
             </a-entity>
            
-            <a-entity collada-model='#plant1' scale="1 1 1"position="10 0 10"></a-entity>
-            <a-entity collada-model='#wartortle' scale="7 7 7"position="-20 0 20" rotation="90 180 0"></a-entity>
+
+            <a-entity id="wartortle2"collada-model='#wartortle' scale="7 7 7"position="-20 0 20" rotation="90 180 0"></a-entity>
            
          
             {seaweed}
             {this.state.bubble}
+
+            <a-image src="#exit-blue" height="15" width="20" color="blue" position="-20 30 15" rotation="0 180 0"></a-image>
             <a-entity>
-              <a-camera wasd-controls="fly: true; acceleration: 150"></a-camera>
+              <a-camera wasd-controls="fly: true; acceleration: 150">
+                <a-cursor color="black" fuse="true" fuse-timeout="1000">
+                  <a-animation begin="fusing" easing="ease" attribute="scale" fill="none" from="1 1 1" to="0 0 0" dur="1000"></a-animation>
+                </a-cursor>
+              </a-camera>
             </a-entity>
            
 
@@ -78,8 +96,41 @@ class Underwater extends React.Component {
       </div>
     )
   }
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  componentDidMount() {
+    this.reversedir1();
+    
+    let goHomePointer = this.goHome;
+    document.querySelector('#wartortle2').addEventListener('click', function() {
+      console.log('I was clicked!');
+      goHomePointer();
+    });
+  }
+    reversedir1() {
+    let reversedir2Pointer = this.reversedir2;
+    setTimeout(function() {
+      document.getElementById("whale-anim")
+      .setAttribute("rotation", "0 180 0");
 
-  
+      document.getElementById("shark-anim")
+      .setAttribute("rotation", "0 180 0");
+      reversedir2Pointer();
+    }, 20000);
+  }
+  reversedir2() {
+    let reversedir1Pointer = this.reversedir1;
+    setTimeout(function() {
+      document.getElementById("whale-anim").setAttribute("rotation", "0 360 0");
+
+      document.getElementById("shark-anim").setAttribute("rotation", "0 360 0");
+      reversedir1Pointer();
+    }, 20000);
+  }
+  goHome() {
+    this.context.router.push("/");
+  }
 }
 
 export default Underwater;
