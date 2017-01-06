382521b855c49f3f689eb969b619c8b5b5942bfa
Cameron Skidmore <cskidz99@gmail.com>
2017-01-05 17:53:17 -0700

    Random waldo placement working. Banner placed behind text.

--- src/components/city.js
+++ src/components/city.js
@@ -10,6 +10,22 @@ class City extends React.Component {
   }
 
   render(){
+    var i = 0;
+    var j = 0
+    var waldo = [];
+    let x, y, z;
+    while(j < 1) {
+      x = Math.random() * 60 -30;
+      y = 0;
+      z = Math.random() * 60 -30;
+      waldo.push(<a-entity key={j} collada-model="#waldo" position={`${x} ${y} ${z}`}></a-entity> );
+      j++;
+    }
+    
+    setTimeout(function() {
+
+    }, 1000)
+
     return (
       <div className="show-vr-div">
 
@@ -28,18 +44,21 @@ class City extends React.Component {
           </a-assets>
 
           <a-sky src="#sky" rotation="0 0 0"></a-sky>
+          <a-plane color="#CCC" height="7" width="60" position="-.67 16.5 -35.35"></a-plane>
+          <a-entity bmfont-text="text: Can you find Waldo?; color: #4a6ee3" position="-26.91 15 -34.63" scale="30 30 30"></a-entity>
 
           <a-entity collada-model="#crowd1" position="-7 -1 -1.5"></a-entity>
           <a-entity collada-model="#crowd2" position="13.82 -1 9.66"></a-entity>
           <a-entity collada-model="#crowd3" position="-7.49 -1 9.63"></a-entity>
           <a-entity collada-model="#crowd4" position="14.77 -1 -1.45"></a-entity>
-          <a-entity id="waldo-model" collada-model="#waldo" position="-5 0 -3"></a-entity>
+          {/*<a-entity id="waldo-model" collada-model="#waldo" position="-5 0 -3"></a-entity>
           
-          {/*<<a-entity collada-model="#future-city" position="-228 0 195"></a-entity>*/}
+          <<a-entity collada-model="#future-city" position="-228 0 195"></a-entity>*/}
+          {waldo}
 
           <a-entity collada-model="#park2" position="29.15 0 26.77"></a-entity>
 
-          <a-camera position="0 0 0" wasd-controls="fly: true">
+          <a-camera position="0 0 0" wasd-controls="fly: false">
             <a-cursor color="yellow" fuse="true" fuse-timeout="3000">
               <a-animation begin="fusing" easing="ease" attribute="scale" fill="none" from="1 1 1" to="0 0 0" dur="3000"></a-animation>
             </a-cursor>
